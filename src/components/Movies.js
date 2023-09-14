e13947884c195df1f1ed5f484e263e6ded0490d1
LUCY <wangecikariu.luc@gmail.com>
2023-09-15 00:35:39 +0300

    Basic routing

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Movies Page</h1>
+  {movies.map((movies) => (
+  <div key= {movies.title}>
+    <h2>Title: {movies.title}</h2>
+    <h3>Time: {movies.time} minutes</h3>
+    <ul>
+      Genre:
+      {movies.genres.map((genre) => (
+        <li key = {genre}>{genre}</li>
+      ))}
+    </ul>
+  </div>
+  ))};
+  </div>;
 }
 
 export default Movies;
